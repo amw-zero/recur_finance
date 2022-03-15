cf7965c6266d54b767f9ebbf95038091b0d95f91
Alex Weisberger <alex@relaypayments.com>
2022-03-15 08:55:21 -0400

    Spec

--- ImplementationSpec.thy
+++ ImplementationSpec.thy
@@ -1,7 +1,26 @@
 theory ImplementationSpec
 
-imports Main
+imports Main AbstractSpec
 
 begin
+
+record new_recurring_transaction =
+  amount :: int
+  name :: string
+
+definition from_new_recurring_transaction :: "new_recurring_transaction \<Rightarrow> int \<Rightarrow> recurring_transaction" where
+"from_new_recurring_transaction nrt last_id = 
+  \<lparr> 
+    id = last_id + 1, 
+    r_amount = amount nrt, 
+    rule = Weekly (ByDay [Mo]),
+    name =  name nrt
+  \<rparr>"
+
+definition create_recurring_transaction :: "recurring_transaction set \<Rightarrow> new_recurring_transaction \<Rightarrow> int \<Rightarrow> recurring_transaction set" where
+"create_recurring_transaction rts new last_id = 
+  AbstractSpec.create_recurring_transaction 
+    (from_new_recurring_transaction new last_id) 
+    rts"
 
 end
